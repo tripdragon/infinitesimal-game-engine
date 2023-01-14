f85c7a9e4c1d6095b2be34a058b19f1fe19f1659
wswoodruff <william.woodruff@standardmagic.com>
2023-01-14 12:30:08 -0500

    Lint, update wordsAdder to style the element w/ js, remove from css

--- System1/Plugins/wordsAdder.js
+++ System1/Plugins/wordsAdder.js
@@ -1,46 +1,54 @@
 
-
-
-
 export class WordsAdder{
-  
+
   element;
   lv = 1;
-  
+
+  _setStyles(element) {
+    element.style.position = "absolute";
+    element.style.top = 0;
+    element.style.right = 0;
+    element.style.padding = "20px";
+    element.style.maxWidth = "700px";
+    element.style.color = "white";
+    element.style.fontSize = "24px";
+    element.style.fontStyle = "normal";
+  }
+
   constructor(element){
-    
+
     this.element = element;
-    
+    this._setStyles(this.element);
+
     let words = this.element;
-    
+
     // cheap cursor
     let text = words.innerText;
     text += "_";
     words.innerText = text;
-    
+
     document.addEventListener('keydown', function(ev) {
       console.log(ev.key);
-      
-      
+
       //let text = words.innerHTML;
       let text = words.innerText;
-      
+
       // cheap cursor
       text = text.slice(0, -1);
-      
+
       if(ev.key === "Backspace"){
         // text = "";
         console.log("Backspace 2222");
         text = text.slice(0, -1);
       }
       else if(ev.key === "Shift"){
-        
+
       }
       // ??????
       else if(ev.key === "Enter"){
         // text = text+"\n";
         console.log("¿¿ enter 222 ¿¿");
-        text = `${text} 
+        text = `${text}
         `;
       }
       else if(ev.key === " "){
@@ -52,16 +60,11 @@ export class WordsAdder{
         //text += ev.key;
         text = `${text}${ev.key}`;
       }
-      
+
       // cheap cursor
       text += "_";
       //words.innerHTML = text;
       words.innerText = text;
-      
-      
     });
   }
-  
-  
-  
 }
