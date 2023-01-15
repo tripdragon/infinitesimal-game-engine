e35df12fff443891c2d79205f6485edce27ccd0a
nhtrd <nickitdesign@gmail.com>
2023-01-15 04:23:31 -0500

    adding keydown to keyboard

--- System1/Modules/input.js
+++ System1/Modules/input.js
@@ -2,6 +2,7 @@ import { addListener } from './listeners.js';
 
 // TODO add gameController
 
+// NEEDS: is keydown
 export function keyboard(config) {
   addListener({ event: 'keydown', func: function(evt) {
     // console.log(evt.key);
@@ -15,4 +16,18 @@ export function keyboard(config) {
       config.any(evt);
     }
   }});
+  
+  addListener({ event: 'keyup', func: function(evt) {
+    // console.log(evt.key);
+    if (typeof config[evt.key] === 'function') {
+      config[evt.key](evt);
+    } else if (typeof config.rest === 'function') {
+      config.rest(evt);
+    }
+
+    if (typeof config.any === 'function') {
+      config.any(evt);
+    }
+  }});
+  
 }
