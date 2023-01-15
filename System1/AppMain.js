a685322558bdb54fab54a783c63ae8e40ed09013
nhtrd <nickitdesign@gmail.com>
2023-01-14 20:27:47 -0500

    make smaller squares

--- System1/AppMain.js
+++ System1/AppMain.js
@@ -27,14 +27,14 @@ export default function AppMain() {
   // Base charactor objects!!
   // here we have a wordy but very direct example of custom scripting for each object square
   // in this example the first arg is gl which is not available yet so well set it in the loop
-  var sq1 = new SquareLike(null, -4, 4, 12, 8);
+  var sq1 = new SquareLike(null, -4, 4, 4, 6);
   sq1.playCode = `return { do : function(obj, helpers){
     obj.x = helpers.randomBetween(-4,4);
     obj.color.x = Math.random();
     obj.color.y = Math.random();
     obj.color.z = Math.random();
   }}`;
-  var sq2 = new SquareLike(null, -4 + -12, 4, 12, 8);
+  var sq2 = new SquareLike(null, -4 + -12, 4, 3, 4);
   sq2.playCode = `return { do : function(obj, helpers){
     // obj.x = helpers.randomBetween(-14,14);
     obj.y = helpers.randomBetween(-14,14);
@@ -42,7 +42,7 @@ export default function AppMain() {
     obj.color.y = Math.random();
     obj.color.z = Math.random();
   }}`;
-  var sq3 = new SquareLike(null, -4 + -12, 4 + -12, 8, 12);
+  var sq3 = new SquareLike(null, -4 + -12, 4 + -12, 2, 5);
   sq3.playCode = `return { do : function(obj, helpers){
     obj.x = helpers.randomBetween(-24,24);
     // obj.y = helpers.randomBetween(-14,14);
